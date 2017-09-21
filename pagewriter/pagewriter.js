836b1f5c38b30ea2b3005559fd14447b52b2db2f
aeroniemi <flightcontrol222@gmail.com>
2017-09-21 18:30:47 +0100

    import from old

---
+++ pagewriter/pagewriter.js
@@ -0,0 +1,73 @@
+var handlebars = require('handlebars'),
+    fs = require('fs');
+const path = require('path')
+var data = {
+   "firs":[
+      {
+         "id":0,
+         "firCode":"LRBB",
+         "firName":"Bucharest",
+         "sfUpdated":"2017 09 20",
+         "sfAirac":"1611",
+         "sfLastPos":"LRBB_L_CTR",
+         "notamAirports":[
+            {
+               ntCode:'LROP',
+               ntName:"Otopeni",
+               ntNotams:'',
+
+            },
+            {
+               ntCode:'LRCL',
+               ntName:"Cluj",
+               ntNotams:'',
+
+            },
+            {
+               ntCode:'LRTR',
+               ntName:"Timisoara",
+               ntNotams:'',
+
+            }
+         ]
+      },
+      {
+         "id":1,
+         "firCode":"LBSR",
+         "firName":"Sofia",
+         "sfUpdated":"2017 09 10",
+         "sfAirac":"1612",
+         "sfLastPos":"LBSR_CTR",
+         "notamAirports":[
+            {
+               "ntCode":'LROP',
+               "ntName":"Otopeni",
+               "ntNotams":'',
+
+            },
+            {
+               "ntCode":'LRCL',
+               "ntName":"Cluj",
+               "ntNotams":'',
+
+            },
+            {
+               "ntCode":'LRTR',
+               "ntName":"Timisoara",
+               "ntNotams":''
+
+            }
+         ]
+      }
+   ],
+
+} ;
+var source = fs.readFileSync(path.join(__dirname, '../../tesseract/views/index.hbs'), 'utf-8');
+var template = handlebars.compile(source);
+var html = template(data);
+fs.writeFile(path.join(__dirname, '../../tesseract/views/index.html'), html, 'utf8',function (err) {
+    if (err) {
+        console.log("failed to save");
+    } else {
+        console.log("succeeded in saving");
+    }});
