0a1dc36b54dc0003a2ff8a2a3868e4f3c6d73b4f
aeroniemi <flightcontrol222@gmail.com>
2017-09-22 16:38:50 +0100

    beautified everything

--- pagewriter/notamloader.js
+++ pagewriter/notamloader.js
@@ -3,87 +3,87 @@ var fs = require('fs');
 var path = require('path');
 var notams = require('notams');
 var data = {
-    "firs":[
-        {
-            "id":0,
-            "firCode":"LRBB",
-            "firName":"Bucharest",
-            "sfUpdated":"2017 09 20",
-            "sfAirac":"1611",
-            "sfLastPos":"LRBB_L_CTR",
-            "notamAirports":[
-                {
-                    ntCode:'LROP',
-                    ntName:"Otopeni",
-                    ntNotams:'',
+	"firs": [
+		{
+			"id": 0,
+			"firCode": "LRBB",
+			"firName": "Bucharest",
+			"sfUpdated": "2017 09 20",
+			"sfAirac": "1611",
+			"sfLastPos": "LRBB_L_CTR",
+			"notamAirports": [
+				{
+					ntCode: 'LROP',
+					ntName: "Otopeni",
+					ntNotams: '',
 
                 },
-                {
-                    ntCode:'LRCL',
-                    ntName:"Cluj",
-                    ntNotams:'',
+				{
+					ntCode: 'LRCL',
+					ntName: "Cluj",
+					ntNotams: '',
 
                 },
-                {
-                    ntCode:'LRTR',
-                    ntName:"Timisoara",
-                    ntNotams:'',
+				{
+					ntCode: 'LRTR',
+					ntName: "Timisoara",
+					ntNotams: '',
 
                 }
             ]
         },
-        {
-            "id":1,
-            "firCode":"LBSR",
-            "firName":"Sofia",
-            "sfUpdated":"2017 09 10",
-            "sfAirac":"1612",
-            "sfLastPos":"LBSR_CTR",
-            "notamAirports":[
-                {
-                    "ntCode":'LBSF',
-                    "ntName":"Otopeni",
-                    "ntNotams": "",
+		{
+			"id": 1,
+			"firCode": "LBSR",
+			"firName": "Sofia",
+			"sfUpdated": "2017 09 10",
+			"sfAirac": "1612",
+			"sfLastPos": "LBSR_CTR",
+			"notamAirports": [
+				{
+					"ntCode": 'LBSF',
+					"ntName": "Otopeni",
+					"ntNotams": "",
 
                 },
-                {
-                    "ntCode":'LBBG',
-                    "ntName":"Cluj",
-                    "ntNotams":"",
+				{
+					"ntCode": 'LBBG',
+					"ntName": "Cluj",
+					"ntNotams": "",
 
                 },
-                {
+				{
 
-                    "ntCode":'LBWN',
-                    "ntName":"Timisoara",
-                    "ntNotams":""
+					"ntCode": 'LBWN',
+					"ntName": "Timisoara",
+					"ntNotams": ""
 
                 }
             ]
         }
     ],
 
-} ;
+};
 
 
-var notamLoad = function() {
-    var callbacks = [];
-    for (var i = 0; i < data.firs.length; i++) {
-        for (var y = 0; y < data.firs[i].notamAirports.length; y++) {
-            var nqAirport = data.firs[i].notamAirports[y];
-            console.log(nqAirport.ntCode);
-            callbacks.push(notams(nqAirport.ntCode, {
-                format: 'ICAO'
-            }));
-        }
-    }
-    console.log(data.firs.length);
-    Promise.all(callbacks).then(notamLoadCompleted);
-    var notamLoadCompleted = function(values) {
-        console.log("it worked");
-        fs.writeFileSync("file.json", JSON.stringify(data));
+var notamLoad = function () {
+	var callbacks = [];
+	for (var i = 0; i < data.firs.length; i++) {
+		for (var y = 0; y < data.firs[i].notamAirports.length; y++) {
+			var nqAirport = data.firs[i].notamAirports[y];
+			console.log(nqAirport.ntCode);
+			callbacks.push(notams(nqAirport.ntCode, {
+				format: 'ICAO'
+			}));
+		}
+	}
+	console.log(data.firs.length);
+	Promise.all(callbacks).then(notamLoadCompleted);
+	var notamLoadCompleted = function (values) {
+		console.log("it worked");
+		fs.writeFileSync("file.json", JSON.stringify(data));
 
-    }
+	}
 }
 notamLoad();
 
--- pagewriter/pagewriter.js
+++ pagewriter/pagewriter.js
@@ -3,72 +3,79 @@ var fs = require('fs');
 var path = require('path');
 
 var data = {
-   "firs":[
+	"firs": [
       {
-         "id":0,
-         "firCode":"LRBB",
-         "firName":"Bucharest",
-         "sfUpdated":"2017 09 20",
-         "sfAirac":"1611",
-         "sfLastPos":"LRBB_L_CTR",
-         "notamAirports":[
+			"id": 0,
+			         "firCode": "LRBB",
+			         "firName": "Bucharest",
+			         "sfUpdated": "2017 09 20",
+			         "sfAirac": "1611",
+			         "sfLastPos": "LRBB_L_CTR",
+			         "notamAirports": [
             {
-               ntCode:'LROP',
-               ntName:"Otopeni",
-               ntNotams:'',
+					ntCode: 'LROP',
+					               ntName: "Otopeni",
+					               ntNotams: '',
 
-            },
-            {
-               ntCode:'LRCL',
-               ntName:"Cluj",
-               ntNotams:'',
 
-            },
-            {
-               ntCode:'LRTR',
-               ntName:"Timisoara",
-               ntNotams:'',
+                },
+             {
+					ntCode: 'LRCL',
+					               ntName: "Cluj",
+					               ntNotams: '',
 
-            }
+
+                },
+             {
+					ntCode: 'LRTR',
+					               ntName: "Timisoara",
+					               ntNotams: '',
+
+
+                }
          ]
-      },
-      {
-         "id":1,
-         "firCode":"LBSR",
-         "firName":"Sofia",
-         "sfUpdated":"2017 09 10",
-         "sfAirac":"1612",
-         "sfLastPos":"LBSR_CTR",
-         "notamAirports":[
+        },
+       {
+			"id": 1,
+			         "firCode": "LBSR",
+			         "firName": "Sofia",
+			         "sfUpdated": "2017 09 10",
+			         "sfAirac": "1612",
+			         "sfLastPos": "LBSR_CTR",
+			         "notamAirports": [
             {
-               "ntCode":'LROP',
-               "ntName":"Otopeni",
-               "ntNotams":{}
+					"ntCode": 'LROP',
+					               "ntName": "Otopeni",
+					               "ntNotams": {}
 
-            },
-            {
-               "ntCode":'LRCL',
-               "ntName":"Cluj",
-               "ntNotams":{}
 
-            },
-            {
-               "ntCode":'LRTR',
-               "ntName":"Timisoara",
-               "ntNotams":{}
+                },
+             {
+					"ntCode": 'LRCL',
+					               "ntName": "Cluj",
+					               "ntNotams": {}
 
-            }
+
+                },
+             {
+					"ntCode": 'LRTR',
+					               "ntName": "Timisoara",
+					               "ntNotams": {}
+
+
+                }
          ]
-      }
+        }
    ],
 
-} ;
+};
 var source = fs.readFileSync(path.join(__dirname, '../../euroscope-tesseract/views/index.hbs'), 'utf-8');
 var template = handlebars.compile(source);
 var html = template(data);
-fs.writeFile(path.join(__dirname, '../../euroscope-tesseract/views/index.html'), html, 'utf8',function (err) {
-    if (err) {
-        console.log("failed to save");
-    } else {
-        console.log("succeeded in saving");
-    }});
+2 fs.writeFile(path.join(__dirname, '../../euroscope-tesseract/views/index.html'), html, 'utf8', function (err) {
+	if (err) {
+		console.log("failed to save");
+	} else {
+		console.log("succeeded in saving");
+	}
+});
