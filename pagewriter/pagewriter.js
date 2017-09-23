5a58963f5b7be936eb5c20cf3befc6451815db25
comp500 <comp500@users.noreply.github.com>
2017-09-23 12:28:58 +0100

    Fix pagewriter with sync (todo async)

--- pagewriter/pagewriter.js
+++ pagewriter/pagewriter.js
@@ -7,12 +7,12 @@ var notamLoader = require('./notamloader.js')
 
 module.exports.formPage = function () {
 	return new Promise(function (resolve, reject) {
-		var data = fs.readFile("data.json");
+		var data = fs.readFileSync("data.json");
 		console.log("fefwave");
-		var source = fs.readFile(path.join(__dirname, '../../euroscope-tesseract/views/index.hbs'), 'utf-8');
+		var source = fs.readFileSync(path.join(__dirname, '../views/index.hbs'), 'utf-8');
 		var template = handlebars.compile(source);
-		var html = template(mike);
-		fs.writeFile(path.join(__dirname, '../../euroscope-tesseract/views/index.html'), html, 'utf8', function (err) {
+		var html = template(JSON.parse(data));
+		fs.writeFile(path.join(__dirname, '../views/index.html'), html, 'utf8', function (err) {
 			if (err) {
 				console.log("failed to save");
 				reject();
